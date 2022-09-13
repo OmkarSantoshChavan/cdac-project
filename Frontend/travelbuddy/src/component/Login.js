9f0663b3cec9ab2f1d2c58158aa97f99f77d6bdb
OmkarSantoshChavan <omkarschavan4@gmail.com>
2022-09-13 16:58:17 +0530

    navigation added

--- Frontend/travelbuddy/src/component/Login.js
+++ Frontend/travelbuddy/src/component/Login.js
@@ -19,8 +19,6 @@ function LoginPage() {
     }
     const { email, password } = customer;
 
-
-
     const FormHandle = e => {
         e.preventDefault();
         addDataToServer(customer)
@@ -29,8 +27,24 @@ function LoginPage() {
     const addDataToServer = (data) => {
         axios.post("http://localhost:8080/login", data).then(
             (response) => {
-                console.log(response);
-                alert(" login Successfully");
+                console.log(response.data);
+                if(response.data.role === "admin")
+                {
+                    localStorage.setItem("loggedinuser",JSON.stringify(response.data));
+                    navigate('/admin');
+                }
+                else if(response.data.role === "owner")
+                {
+                    localStorage.setItem("loggedinuser",JSON.stringify(response.data));
+                    navigate('/owner');
+                }
+                else if(response.data.role === "customer")
+                {
+                    localStorage.setItem("loggedinuser",JSON.stringify(response.data));
+                    navigate('/customer');
+                }
+
+                //alert(" login Successfully");
             }, (error) => {
                 console.log(error);
                 alert("Invalid credentials !!!");
--- Frontend/travelbuddy/src/component/Role/Admin.js
+++ Frontend/travelbuddy/src/component/Role/Admin.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 import './Style.css';
 
 function Admin(props) {
 
     let { data } = props;
 
+    const navigate = useNavigate();
+
+    const handleProfile = () => {
+        navigate(`/edit_profile`);
+    }
+
     const footer = (
         <span>
-            <Button className='profile-button' label="Edit Profile" />
+            <Button className='profile-button' label="Edit Profile" onClick={handleProfile} />
             <Button className='profile-button' label="Remove User" />
             <Button className='profile-button' label="Remove Property" />
 
