78884e0ce077fda3daa6e5b40ee34333eb9c8efd
OmkarSantoshChavan <omkarschavan4@gmail.com>
2022-09-15 13:08:19 +0530

    rgwegr

--- travelbuddy/test/src/test/java/com/example/demo/TestApplicationtests.java
+++ travelbuddy/test/src/test/java/com/example/demo/TestApplicationtests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.example.demo.*")
-public class TestApplication {
+public class TestApplicationtests {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
